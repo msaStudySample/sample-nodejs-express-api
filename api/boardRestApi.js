39c516f2bf33ec7c4280226566b6ef1c988ddb17
서명석(Myeongseok Seo) 과장 낙스넷 <myeongseok1.seo@doosan.com>
2020-10-16 22:14:36 +0900

    board rest api

--- api/boardRestApi.js
+++ api/boardRestApi.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
-// const mdbConn = require('../model/mariaDBConn')
+// const mdbConn = require('../db/mariaDBConn')
 const Board = require('../models').board
-
 
 // router.get('/api/v1/boardList', (req, res) => {
 // 	mdbConn.getBoardList()
@@ -10,10 +9,67 @@ const Board = require('../models').board
 // 		.catch((err) => { console.error(err) })
 // })
 
-router.get('/api/v1/boards', (req, res) => {
+router.get('/boards', (req, res, next) => {
 	Board.findAll()
-		.then((rows) => { res.json(rows)})
-		.catch((err) => { console.error(err) })
+		.then((wrts) => { res.json(wrts)})
+		.catch((err) => {
+			console.error(err)
+			next(err)
+		})
 })
+
+router.get('/boards/:wrt_seq', (req, res, next) => {
+	Board.findOne(
+		{ wrt_seq: req.params.wrt_seq}
+	)
+		.then((wrt) => {
+			res.json(wrt)
+		})
+		.catch((err) => {
+			console.error(err)
+			next(err)
+		})
+})
+
+router.post('/boards', (req, res, next) => {
+	Board.create({
+		title: req.body.title,
+		contents: req.body.contents,
+		reg_id: req.body.reg_id
+	})
+		.then((result) => {
+			res.status(201).json(result)
+		})
+		.catch((err) => {
+			console.error(err)
+			next(err)
+		})
+})
+
+router.patch('/boards/:wrt_seq', (req, res, next) => {
+	Board.update(
+		{ title: req.body.title},
+		{ where: {wrt_deq: req.params.wrt_seq}}
+		)
+		.then((result) => {
+			res.json(result)
+		})
+		.catch((err) => {
+			console.log(err)
+			next(err)
+		})
+})
+
+router.delete('/board/:wrt_seq', ((req, res, next) =>
+	Board.delete({ wrt_seq: req.params.wrt_seq})
+		.then((result) => {
+			res.json(result)
+		})
+		.catch((err) => {
+			console.log(err)
+			next(err)
+		})
+))
+
 
 module.exports = router
--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const sequelize = require('./models').sequelize
-
+const cors = require('cors');
 const app = express()
 const port = 3000
-sequelize.sync()
 
+
+sequelize.sync()
 
 app.get('/', (req,res) => {
 	res.send('Hello World!!!')
 })
 
-app.use('/', require('./api/boardRestApi'))
+app.use(cors())
+app.use('/api/v1', require('./api/boardRestApi'))
 
 app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`)
+	console.log(`Example app listening at http://127.0.0.1:${port}`)
 })
 
 
